09c0cd1e075a9ad408a5469ce34b513191522e45
eliorNourani <elior2031@gmail.com>
2020-08-28 00:11:37 +0300

    add the questions to user, and create 2 first row in board.

--- C20 Ex02 Guy 200118602 Elior 203136916/UI/Board.cs
+++ C20 Ex02 Guy 200118602 Elior 203136916/UI/Board.cs
@@ -1,14 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
-using System.Threading.Tasks;
+using Ex02.ConsoleUtils;
+
 
 namespace UI
 {
     class Board
     {
+        private int m_Col;
+        private int m_Row;
+        private string[,] m_BoardMat;
+        private StringBuilder m_Shave = new StringBuilder(" ");
+        private StringBuilder m_BoardToPrint =new StringBuilder("");
+        
 
+        public Board(int i_Size)
+		{
+            m_Col = i_Size * 4 + 2;
+            m_Row = i_Size * 2 + 2;
+            m_BoardMat = new string[m_Row, m_Col];
+            
+            for(int i=0;i<m_Col -1;i++)
+			{
+                m_Shave.Append("=");
+			}
+            for(int i = 0; i<m_Row;i++)
+			{
+                if(i==0)
+				{
+                    char bigLetter = 'A';
+                    StringBuilder column = new StringBuilder("");
+                    for(int j = 0; j < m_Col/4 ; j++)
+					{
+                        column.Append("   ");
+                        column.Append(bigLetter);
+                        bigLetter = (char)(((int)bigLetter) + 1);
+					}
+					m_BoardToPrint.AppendLine(column.ToString());
+                }
+                else if (i%2==1)
+				{
+                    m_BoardToPrint.AppendLine(m_Shave.ToString());  
+				}
+				else
+				{
+                    
+                }
+                
+			}
+
+        }
+        public void ShowBoard()
+		{
+            Screen.Clear(); //clear screen
+            //col = size of array need to m_size * 4 + 2
+            //row = size of array need to m_size * 2 + 2
+            Console.WriteLine(m_BoardToPrint);
+
+            
+        }
         
     }
 }
---
+++ C20 Ex02 Guy 200118602 Elior 203136916/UI/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace UI
+{
+	class Program
+	{
+
+		public static void Main()
+		{
+
+			runningGameCheckers();
+		}
+
+		private static void runningGameCheckers()
+		{
+			NewGame game = new NewGame();
+			Board board = new Board(game.SizeBoard);
+			board.ShowBoard();
+
+		}
+
+
+	}
+
+	class NewGame
+	{
+		private String m_UserName;
+		private String m_UserName2;
+		private int m_SizeBoard;
+		private bool m_TwoPlayers;
+
+		public NewGame()
+		{
+
+			m_UserName = getFromUserTheUserName();
+			m_SizeBoard = getFromUserTheSizeOfBoard();
+			m_TwoPlayers = getFromUserIfHaveTwoPlayersInTheGame();
+			if (m_TwoPlayers)
+			{
+				m_UserName2 = getFromUserTheUserName();
+			}
+
+		}
+
+		private bool getFromUserIfHaveTwoPlayersInTheGame()
+		{
+			bool twoPlayers = false;
+			int players;
+			do
+			{
+				Console.Write("please choose how many player have in the game(1/2):");
+
+			} while (!int.TryParse(Console.ReadLine(), out players) && (players == 1 || players == 2));
+
+			if (players == 2)
+			{
+				twoPlayers = true;
+			}
+			return twoPlayers;
+		}
+
+		private int getFromUserTheSizeOfBoard()
+		{
+
+			int size;
+			do
+			{
+				Console.Write("Please choose size of board (6,8,10):");
+				int.TryParse(Console.ReadLine(), out size);
+
+			} while (size != 6 && size != 8 && size != 10);
+
+			return size;
+		}
+
+		private string getFromUserTheUserName()
+		{
+			String name;
+			do
+			{
+				Console.WriteLine("Please enter your user name (please the name was without spaces and up to 20):");
+				name = Console.ReadLine();
+
+			} while (name.Length >= 20 || name.Contains(" ") || name.Length < 1);
+
+
+			return name;
+		}
+		public int SizeBoard
+		{
+			get{ return m_SizeBoard; }
+			
+		}
+
+	}
+}
